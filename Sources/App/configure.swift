3e9cda5e622d16c404bd2be28a7434248705ea65
Andreas Felder <andrfelder@student.ethz.ch>
2023-12-05 15:30:29 +0100

    adding heroku build files again

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -10,14 +10,28 @@ public func configure(_ app: Application) async throws {
     app.middleware.use(app.sessions.middleware)
     app.middleware.use(User.sessionAuthenticator())
     
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
+    if var config = Environment.get("DATABASE_URL")
+        .flatMap(URL.init)
+        .flatMap(SQLPost.init) {
+      config.tlsConfiguration = .forClient(
+        certificateVerification: .none)
+      app.databases.use(.postgres(
+        configuration: config
+      ), as: .psql)
+    } else {
+      app.databases.use(
+        .postgres(
+          hostname: Environment.get("DATABASE_HOST") ??
+            "localhost",
+          port: databasePort,
+          username: Environment.get("DATABASE_USERNAME") ??
+            "vapor_username",
+          password: Environment.get("DATABASE_PASSWORD") ??
+            "vapor_password",
+          database: Environment.get("DATABASE_NAME") ??
+            databaseName),
+        as: .psql)
+    }
 
     app.views.use(.leaf)
     
